617da12ee75ff75225b63f5f31b54c8fcff6b448
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-02 16:40:01 +0500

    Add function to convert strings to uppercase

---
+++ app.js
@@ -0,0 +1,42 @@
+function makeUpperCase(str) {
+    let result = str.toUpperCase();
+    return result;
+}
+
+let text1 = "salom";
+let text2 = "javascript";
+let text3 = "dasturlash";
+let text4 = "hello world";
+let text5 = "nodejs";
+
+let res1 = makeUpperCase(text1);
+let res2 = makeUpperCase(text2);
+let res3 = makeUpperCase(text3);
+let res4 = makeUpperCase(text4);
+let res5 = makeUpperCase(text5);
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+
+console.log(makeUpperCase("olma"));
+console.log(makeUpperCase("anor"));
+console.log(makeUpperCase("uzum"));
+console.log(makeUpperCase("shaftoli"));
+console.log(makeUpperCase("banan"));
+
+let a = makeUpperCase("kitob");
+let b = makeUpperCase("daftar");
+let c = makeUpperCase("qalam");
+let d = makeUpperCase("ruchka");
+let e = makeUpperCase("sumka");
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log("Dastur tugadi");
